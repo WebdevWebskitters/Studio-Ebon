336c150b7e6a1423ea8650c6364a3d5ff611ecf2
WebdevWebskitters <161023802+WebdevWebskitters@users.noreply.github.com>
2024-03-08 19:56:32 +0530

    Update common.js

--- common.js
+++ common.js
@@ -743,7 +743,7 @@ function pageScript() {
 
         let circleAnimation = gsap.to(circle, {
             translateX: `${halfWidth - halfCircleWidth}px`,
-            translateY:`${circle.offsetHeight / 3}px`,
+            translateY: `${circle?.offsetHeight / 3}px`,
             scale: 0.8,
             ease: 'none',
             scroller: isDekstop ? pageContainer : window,
@@ -771,8 +771,8 @@ function pageScript() {
                 trigger: el,
                 start: "top 60%",
                 end: () => "bottom 60%",
-                pin:circleOuter,
-                pinSpacing:false,
+                pin: circleOuter,
+                pinSpacing: false,
                 scroller: isDekstop ? pageContainer : window,
                 onEnter: function () {
                     addRemove(i);
@@ -817,7 +817,7 @@ function pageScript() {
         // var accPanel = document.getElementsByClassName('acc_bttn');
         for (let i = 0; i < accBtn.length; i++) {
             accBtn[i].addEventListener('click', toggleItem, false);
-            accBtn[i].addEventListener('click', function(){
+            accBtn[i].addEventListener('click', function () {
                 locoScroll.update();
             });
         }
@@ -829,6 +829,125 @@ function pageScript() {
             if (itemClass == 'acc_item close') {
                 this.parentNode.className = 'acc_item open';
             }
+        }
+
+        // Random Positioning Gallery
+        const imgGallery = document?.querySelectorAll("[random_gallery]");
+        let contW  = document?.querySelector(".img_wrppr").getBoundingClientRect().width;
+
+        if (imgGallery.length > 0) {
+            imgGallery.forEach((imgWrapper, index) => {
+                const list = imgWrapper?.querySelector(".img_lstng");
+                const item = list?.querySelectorAll(".img_hldr");
+                const totalLn = item.length;
+                var count = parseInt(imgWrapper?.getAttribute("random_gallery"), 0);
+                var duration = Number(imgWrapper?.getAttribute("data-time"));
+                gsap.set(imgWrapper,{
+                    width: `${Math.round(((contW - imgWidth - imgWidth / (imgWrppr.length * 4)) / contW) * 100) / 2}%`,
+                })
+                if (isNaN(count)) {
+                    count = Math.floor(totalLn / 2);
+                }
+                if (duration == 0) {
+                    duration = 1;
+                }
+                // console.log(count, duration);
+                var k = 0;
+                var mainTl = gsap.timeline({
+                    pause: true,
+                    defaults: {
+                        duration: duration,
+                    },
+                }),
+                    subTl = gsap.timeline({
+                        pause: true,
+                        repeatRefresh: true,
+                        repeat: -1,
+                        defaults: {
+                            duration: duration,
+                        },
+                    });
+
+                if (count + 2 < totalLn) {
+                    [...item].map((el, i) => {
+                        //primary setup
+                        gsap.set(el, {
+                            opacity: 0,
+                            position: "absolute",
+                            left: 0,
+                            top: 0,
+                            x: getRandomNumber(0, list.clientWidth - el.clientWidth / 2),
+                            y: getRandomNumber(0, list.clientHeight - el.clientHeight / 2),
+                        });
+
+                        if (i >= 0 && i < count) {
+                            k = count + i;
+                        }
+                        if (k > totalLn - 2) {
+                            k = -1;
+                        }
+                        k++;
+                        //console.log(i, ">>", k, i);
+
+                        subTl
+                            .to(item[k], {
+                                opacity: 1,
+                            })
+                            .to(
+                                item[i],
+                                {
+                                    opacity: 0,
+                                },
+                                "<"
+                            )
+                            .set(item[i], {
+                                x: getRandomNumber(0, list.clientWidth - item[i].clientWidth / 2),
+                                y: getRandomNumber(0, list.clientHeight - item[i].clientHeight / 2),
+                            });
+                    });
+                    subTl.pause();
+
+                    //for first time show
+                    for (let i = 0; i <= count; i++) {
+                        mainTl.set(item[i], { opacity: 0 }).to(item[i], {
+                            opacity: 1,
+                            onComplete: () => {
+                                if (i == count) {
+                                    subTl.restart();
+                                }
+                            },
+                        });
+                    }
+                    mainTl.pause();
+                }
+
+                gsap.set(list, {
+                    opacity: 0,
+                    pointerEvents: "none",
+                });
+                const btn = imgWrapper.querySelector("[data-gallery_btn]");
+                btn.addEventListener("mouseenter", () => {
+                    mainTl.pause().progress(0);
+                    subTl.pause().progress(0);
+                    gsap.to(list, {
+                        opacity: 1,
+                        pointerEvents: "all",
+                        onComplete: () => mainTl.restart(),
+                    });
+                });
+
+                document.addEventListener("mouseleave", () => {
+                    mainTl.pause().progress(0);
+                    subTl.pause().progress(0);
+                    gsap.to(list, {
+                        opacity: 0,
+                        pointerEvents: "none",
+                    });
+                });
+            });
+        }
+        function getRandomNumber(min, max) {
+            return Math.random() * (max - min) + min;
         }
         //end ready
 
