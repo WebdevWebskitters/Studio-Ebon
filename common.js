f9b9ef7967129017face298ae7bdee3fc3dd26a4
WebdevWebskitters <161023802+WebdevWebskitters@users.noreply.github.com>
2024-03-13 11:41:38 +0530

    Update common.js

--- common.js
+++ common.js
@@ -962,10 +962,13 @@ function pageScript() {
                 $(this).parents('.srch_slct').find('.srch_slct_tggle').children('.srch_txt_block').text($(this).next('span').text());
             }
         });
-        document?.querySelector('.rdio_cstm')?.addEventListener("click", () => {
+        document.querySelector('.rdio_cstm')?.addEventListener("click", () => {
             locoScroll.update();
         })
-        document?.querySelector('.custom_lnk > input[type=radio]')?.addEventListener("click", () => {
+        document.querySelector('.custom_lnk > input[type=radio]')?.addEventListener("click", () => {
+            locoScroll.update();
+        })
+        document.querySelector('.slct_item')?.addEventListener("click", () => {
             locoScroll.update();
         })
         // Loop Scroll
