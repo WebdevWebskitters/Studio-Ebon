4042215b32c3c26136851986949d94f98ff5e412
WebdevWebskitters <161023802+WebdevWebskitters@users.noreply.github.com>
2024-03-14 16:33:35 +0530

    Update common.js

--- common.js
+++ common.js
@@ -146,7 +146,7 @@ function pageScript() {
             el.appendChild(copy);
             // copy.insertBefore(hdngInner);
         })
-         // Clone Text And Append 2
+        // Clone Text And Append 2
         let hdngMain2 = document.querySelectorAll('.mrque_dtls_sldr_alt');
         hdngMain2.forEach((el, i) => {
             let hdngInner = document.querySelector('.mrque_dtls_sldr_alt .mrquee_item');
@@ -154,14 +154,14 @@ function pageScript() {
             el.appendChild(copy);
             // copy.insertBefore(hdngInner);
         })
-         // Clone Text And Append 2
-         let hdngMain3 = document.querySelectorAll('.mrquee_spkng_sldr');
-         hdngMain3.forEach((el, i) => {
-             let hdngInner = document.querySelector('.mrquee_spkng_sldr .mrquee_item');
-             let copy = hdngInner.cloneNode(true);
-             el.appendChild(copy);
-             // copy.insertBefore(hdngInner);
-         })
+        // Clone Text And Append 2
+        let hdngMain3 = document.querySelectorAll('.mrquee_spkng_sldr');
+        hdngMain3.forEach((el, i) => {
+            let hdngInner = document.querySelector('.mrquee_spkng_sldr .mrquee_item');
+            let copy = hdngInner.cloneNode(true);
+            el.appendChild(copy);
+            // copy.insertBefore(hdngInner);
+        })
         ///////// init function //////////////////
         function loadInit() {
             //locomotive & scrolltrigger refresh
@@ -1123,6 +1123,130 @@ function pageScript() {
                 }
             }
         });
+        // Random Positioning Click Gallery
+        const clickGallery = document.querySelectorAll("[click_gallery]");
+        if (clickGallery.length > 0) {
+            clickGallery.forEach((imgWrapper) => {
+                const list = imgWrapper.querySelector("ul");
+                const item = list.querySelectorAll("li");
+                const totalLn = item.length;
+                var count = parseInt(imgWrapper.getAttribute("click_gallery"), 0);
+                var duration = Number(imgWrapper.getAttribute("data-time"));
+
+                if (count + 1 < totalLn) {
+                    if (isNaN(count)) {
+                        count = Math.floor(totalLn / 2);
+                    }
+                    if (duration == 0) {
+                        duration = 1;
+                    }
+                    i = count;
+
+                    var i = 0;
+                    var k = 0;
+
+                    //primary setup
+                    [...item].map((el, i) => {
+                        gsap.set(el, {
+                            opacity: 0,
+                            position: "absolute",
+                            left: 0,
+                            top: 0,
+                            scale: 0.8,
+                            transformOrighin: "50% 50%",
+                            zIndex: 0,
+                            width: gsap.utils.random(35, 48, 5) + "%",
+                            // width:
+                            //   (getRandomNumber(
+                            //     window.innerWidth / count,
+                            //     window.innerWidth / 2
+                            //   ) /
+                            //     window.innerWidth) *
+                            //     100 +
+                            //   "%",
+                        });
+                        if (i < count) {
+                            let center = {
+                                x: window.innerWidth / 2 - el.clientWidth / 2,
+                                y: window.innerHeight / 2 - el.clientHeight / 2,
+                                xOffset: window.innerWidth / (count * 2),
+                                yOffset: window.innerHeight / (count * 2),
+                            };
+                            gsap.set(el, {
+                                opacity: 1,
+                                x:
+                                    center.x +
+                                    gsap.utils.random(
+                                        -center.xOffset,
+                                        center.xOffset,
+                                        center.xOffset / 2
+                                    ),
+                                y:
+                                    center.y +
+                                    gsap.utils.random(
+                                        -center.yOffset,
+                                        center.yOffset,
+                                        center.yOffset / 2
+                                    ),
+                                scale: 1,
+                                transformOrighin: "50% 50%",
+                                zIndex: 2,
+                            });
+                            el.classList.add("active");
+                        }
+                    });
+
+                    function callImg(i, pos) {
+                        gsap.set(item, {
+                            zIndex: 0,
+                        });
+                        gsap.set(item[i], {
+                            x: pos.x - item[i].clientWidth / 2,
+                            y: pos.y - item[i].clientHeight / 2,
+                            scale: 0.8,
+                            zIndex: 1,
+                        });
+                        gsap.to(item[i], {
+                            opacity: 1,
+                            scale: 1,
+                            transformOrighin: "50% 50%",
+                            ease: "Power3.easeOut",
+                        });
+                        item[i].classList.add("active");
+
+                        if (list.querySelectorAll("li.active").length > count) {
+                            if (i - count >= 0) {
+                                k = i - count;
+                            } else {
+                                k = k + 1;
+                                if (k > totalLn) {
+                                    k = 0;
+                                }
+                            }
+                            // console.log(k);
+                            gsap.to(item[k], {
+                                opacity: 0,
+                                scale: 0.8,
+                                ease: "Power3.easeOut",
+                            });
+                            item[k].classList.remove("active");
+                        }
+                    }
+
+                    gsap.set(list, {
+                        opacity: 1,
+                    });
+                    imgWrapper.addEventListener("click", (e) => {
+                        var pos = { x: e.clientX, y: e.clientY };
+                        callImg(i, pos);
+                        i++;
+                        if (i >= totalLn) {
+                            i = 0;
+                        }
+                    });
+                }
+            });
+        }
         //end ready
 
         //// page loader
